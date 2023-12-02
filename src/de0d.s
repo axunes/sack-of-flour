c33c71b373ba5382dd2d5ddceb9b057121c8c4d7
axunes <55267708+axunes@users.noreply.github.com>
2023-12-02 07:17:19 -0500

    Update de0d.s

--- src/de0d.s
+++ src/de0d.s
@@ -4,44 +4,11 @@ CA65HL_USE_CUSTOM_SYNTAX = 0
 .include "ca65hl/ca65hl.inc"
 
 .segment "PRGE"
-; label_de0d:
-; 	lda #$00
-; 	sta idx
-; label_de12:
-; 	lda #$00
-; 	sta ram_87
-; label_de17:
-; 	inc ram_87
-; 	lda #$40
-; 	sta temp
-; 	lda ram_87
-; 	cmp temp
-; 	beq :+
-; 	jmp label_de17
-; :	inc idx
-; 	lda #$11
-; 	sta temp
-; 	lda idx
-; 	cmp temp
-; 	beq :+
-; 	jmp label_de12
-; :	rts
-
-
-
 label_de0d:
 	for (lda #0 : sta idx, idx <> #17, inc idx)
-		label_de12:
-			lda #$00
-			sta ram_87
-		label_de17:
-			inc ram_87
-			lda #$40
-			sta temp
-			lda ram_87
-			cmp temp
-			beq :+
-			jmp label_de17
-		:
+		for (lda #0 : sta ram_87, ram_87 <> #64, inc ram_87)
+		; nothing? lol
+		next
 	next
+	
 	rts
