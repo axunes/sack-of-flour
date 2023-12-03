edda59083007b4dec851d3ddd154ee1fc7affbae
axunes <55267708+axunes@users.noreply.github.com>
2023-12-03 06:07:41 -0500

    Update dc92.s

--- src/dc92.s
+++ src/dc92.s
@@ -42,45 +42,32 @@ label_dc92:
 
 
 label_dd2d:
-	lda #$00
-	sta ram_83
-label_dd32:
-	ldx ram_83
-	lda $8044,x
-	sta ram_84
-	ldx ram_83
-	lda $8049,x
-	sta ram_85
-	if (player_position_y_again - #2 >> 4 = ram_85), long
-		if (player_pos_x1 = ram_84 && player_pos_x2 <= #4) jmp label_ddb0
-		if (player_pos_x1 + #1 = ram_84 && player_pos_x2 >= #12) jmp label_ddb0
-	endif
-label_dd9b:
-	inc ram_83
-	lda ram_7e
-	sta temp
-	lda ram_83
-	cmp temp
-	bpl :+
-	jmp label_dd32
-:	rts
+	for (lda #0 : sta ram_83, ram_83 <= ram_7e, inc ram_83)
+		ldx ram_83
+		lda $8044,x
+		sta ram_84
+		ldx ram_83
+		lda $8049,x
+		sta ram_85
+
+		if (player_position_y_again - #2 >> 4 = ram_85), long
+			if (player_pos_x1 = ram_84 && player_pos_x2 <= #4) jmp label_ddb0
+			if (player_pos_x1 + #1 = ram_84 && player_pos_x2 >= #12) jmp label_ddb0
+		endif
+	next
+	rts
+
 label_ddb0:
 	jsr label_c3eb
-	lda #$00
-	sta ram_86
-label_ddb8:
-	jsr label_db8e
-	jsr label_e679
-	jsr label_de0d
-	inc ram_86
-	lda #$15
-	sta temp
-	lda ram_86
-	cmp temp
-	beq :+
-	jmp label_ddb8
-:	jsr label_c3eb
+	for (lda #0 : sta ram_86, ram_86 <> #21, inc ram_86)
+		jsr label_db8e
+		jsr label_e679
+		jsr label_de0d
+	next
+
+	jsr label_c3eb
 	rts
+
 label_ddd8:
 	ldx ram_7f
 	lda #$01
